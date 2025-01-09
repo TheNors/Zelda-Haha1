39f5a0a4b336c2653e4ba0e748857d604b5f8e9b
Konna-Toad <smahopperstad91@gmail.com>
2025-01-09 11:17:23 +0100

    Movement

--- My project (10)/Assets/Scripts/PlayerMovement.cs
+++ My project (10)/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical"); 
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);   
     }
 }
